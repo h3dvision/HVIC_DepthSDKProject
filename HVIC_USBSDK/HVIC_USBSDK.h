ba81d03e3ae224d9f0c73f8829beca5e9f01ad75
hcchangH3d <hc.chang@h3dvision.com>
2022-12-26 22:11:35 +0800

    1. revise for qvga rgb output

--- HVIC_USBSDK/HVIC_USBSDK.h
+++ HVIC_USBSDK/HVIC_USBSDK.h
@@ -20,12 +20,14 @@ extern "C" {
 	HVIC_USBSDKDLL bool HVIC_IniDepthAndOpenDefaultQ();
 	HVIC_USBSDKDLL bool HVIC_GetXyzIR(std::vector<float>& xyzir);
 	HVIC_USBSDKDLL bool HVIC_SaveDepthPly();
+	HVIC_USBSDKDLL bool HVIC_SaveDepthPlyQ();
 	HVIC_USBSDKDLL bool HVIC_ReleaseDepthAndClose();
 	//
 	HVIC_USBSDKDLL bool HVIC_IniRGBDAndOpenDefault();
 	HVIC_USBSDKDLL bool HVIC_IniRGBDAndOpenDefaultQ();
 	HVIC_USBSDKDLL bool HVIC_GetXyzRgb(std::vector<float>& xyz, unsigned char* rgb);
 	HVIC_USBSDKDLL bool HVIC_SaveRgbDPly();
+	HVIC_USBSDKDLL bool HVIC_SaveRgbDPlyQ();
 	HVIC_USBSDKDLL bool HVIC_ReleaseRgbDAndClose();
     
 	HVIC_USBSDKDLL bool HVIC_XyziToColorIrZ(std::vector<float> xyza, unsigned char* color, unsigned char* irz);
